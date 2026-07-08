414d03577305f071f664eb3bff03c33238f6a59d
Yegane Jahanbani <ygn.jhi8@gmail.com>
2026-07-08 22:10:15 +0330

    feat: implement User and Profile Mongoose schemas with relational referencing

---
+++ src/models/Profile.js
@@ -0,0 +1,62 @@
+// models/Profile.js
+import { Schema, model, models } from "mongoose";
+
+const profileSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    realState: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    constructionDate: {
+      type: Date,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["villa", "apartment", "store", "office"],
+    },
+    amenities: {
+      type: [String],
+      default: [],
+    },
+    rules: {
+      type: [String],
+      default: [],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    published: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Profile = models.Profile || model("Profile", profileSchema);
+export default Profile;
