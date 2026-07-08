82bb32df67f7530a6fdc49d580df0bf594f35bef
Yegane Jahanbani <ygn.jhi8@gmail.com>
2026-07-08 22:38:13 +0330

    feat: implement MongoDB connection utility with active session caching

---
+++ src/utils/connectDB.js
@@ -0,0 +1,10 @@
+// utils/connectDB.js
+import mongoose from "mongoose";
+
+export default async function connectDB() {
+    if(mongoose.connections[0].readyState) return
+    mongoose.set("strictQuery",false)
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connect to DB")
+    
+}
