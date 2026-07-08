7656fdc292993ada8947d6c13e79d880aa46fc3a
Yegane Jahanbani <ygn.jhi8@gmail.com>
2026-07-08 23:26:33 +0330

    feat: implement NextAuth session provider for client-side authentication context

---
+++ src/providers/NextAuthProvider.js
@@ -0,0 +1,6 @@
+// providers/NextAuthProvider.js
+"use client";
+import { SessionProvider } from "next-auth/react";
+export default function NextAuthProvider({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
