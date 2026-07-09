0cd53dbd02976296ccd55d06c7a9a081dc2e0bf6
Yegane Jahanbani <ygn.jhi8@gmail.com>
2026-07-09 21:55:25 +0330

    feat: implement secure admin dashboard route with database-level role verification

---
+++ src/app/admin/page.js
@@ -0,0 +1,25 @@
+// app/admin/page.js
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import User from "@/models/User";
+import DashboardSidebar from "@/layout/DashboardSidebar";
+import AdminPage from "@/template/AdminPage";
+import Profile from "@/models/Profile";
+
+export default async function Admin() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/");
+
+  const user = await User.findOne({ email: session.user.email });
+  if (user.role !== "ADMIN") redirect("/dashboard");
+
+  const profiles = await Profile.find({ published: false });
+  return (
+    <DashboardSidebar role={user.role} email={user.email}>
+      <AdminPage profiles={profiles} />
+    </DashboardSidebar>
+  );
+}
